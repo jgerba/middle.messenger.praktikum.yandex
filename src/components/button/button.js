6ab60c2bb623adf14b583f9399a0a68f32968a4a
Julius Gerba <juliusgerba@gmail.com>
2024-02-04 22:15:07 +0300

    upd

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,1 +1,2 @@
-export default `<button class='btn {{class}} {{#if extra-btn}}btn--extra{{/if}} {{#if warning-btn}}btn--warning{{/if}}' type='{{#if submit}}submit{{else}}button{{/if}}'>{{text}}</button>`;
+export default `
+<button class='btn {{class}} {{#if extra-btn}}btn--extra{{/if}} {{#if warning-btn}}btn--warning{{/if}}' type='{{#if submit}}submit{{else}}button{{/if}}'>{{text}}</button>`;
