ef24615ef7e1c546de8b57f3b0734019cf19bf05
Julius Gerba <juliusgerba@gmail.com>
2024-02-04 10:13:08 +0300

    return btn added

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './button/button.js';
 export { default as Input } from './input/input.js';
+export { default as ReturnBtn } from './returnButton/returnButton.js';
 export { default as SignInForm } from './forms/signIn/signInForm.js';
 export { default as SignUpForm } from './forms/signUp/signUpForm.js';
---
+++ src/components/returnButton/returnButton.js
@@ -0,0 +1,7 @@
+export default `
+<div class='btn-return-wrapper'>
+  <button class='btn-return {{class}}'><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960"
+      width="24">
+      <path fill="#ffffff" d="M360-240 120-480l240-240 56 56-144 144h568v80H272l144 144-56 56Z" />
+    </svg></button>
+</div>`;
