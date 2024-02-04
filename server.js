fa6a778ba1eee60e224fc4f2323fe93a40ec8239
Julius Gerba <juliusgerba@gmail.com>
2024-02-04 17:15:59 +0300

    server added

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+const publicDir = path.join(process.cwd(), '/dist');
+
+app.use(express.static(publicDir));
+
+app.get('/*', (req, res) => {
+    res.sendFile(publicDir + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
