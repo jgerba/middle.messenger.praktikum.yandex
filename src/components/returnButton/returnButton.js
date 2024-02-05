4e044fd3b8b22f6a40c879c0e8a5ecc30116490f
Julius Gerba <juliusgerba@gmail.com>
2024-02-05 23:00:52 +0300

    inline svg removed & upds

--- src/components/returnButton/returnButton.js
+++ src/components/returnButton/returnButton.js
@@ -1,4 +1,4 @@
 export default `
 <div class='btn-return-wrapper'>
-  <button class='btn-return {{class}}'><img src='../static/svg/arrow.svg' alt='send icon'></button>
+  <button class='btn-return {{class}}'><img src='../static/svg/arrow.svg' alt='return'></button>
 </div>`;
