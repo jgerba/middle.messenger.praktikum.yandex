e3a5072de8deb1468f7e98cf0fd92b86527685c7
Julius Gerba <juliusgerba@gmail.com>
2024-02-05 21:27:55 +0300

    link upd

--- src/pages/error/error.js
+++ src/pages/error/error.js
@@ -2,6 +2,6 @@ export default `
 <main class="main main-error-page">
   <h1>{{#if notFound}}404{{else}}500{{/if}}</h1>
   <h3>{{#if notFound}}Wrong path{{else}}We are already solving the problem{{/if}}</h3>
-  <a href="/chatspage">Back to chats</a>
+  <a href="/chatpage">Back to chats</a>
 </main>
 `;
