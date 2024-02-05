6b1d3aa39377d2d9094c0fe2009b823837554af6
Julius Gerba <juliusgerba@gmail.com>
2024-02-05 22:54:48 +0300

    upd

--- src/components/returnButton/returnButton.js
+++ src/components/returnButton/returnButton.js
@@ -1,7 +1,4 @@
 export default `
 <div class='btn-return-wrapper'>
-  <button class='btn-return {{class}}'><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960"
-      width="24">
-      <path fill="#ffffff" d="M360-240 120-480l240-240 56 56-144 144h568v80H272l144 144-56 56Z" />
-    </svg></button>
+  <button class='btn-return {{class}}'><img src='../static/svg/arrow.svg' alt='send icon'></button>
 </div>`;
