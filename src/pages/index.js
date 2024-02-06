e6c1dff1508d72c8c17e4fc6b07cba9f2c448342
Julius Gerba <juliusgerba@gmail.com>
2024-02-06 19:06:39 +0300

    chat page dummy added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export { default as profile } from './profile/profile.js';
 export { default as changeprofile } from './changeProfile/changeProfile.js';
 export { default as authpage } from './auth/auth.js';
+export { default as chatpage } from './chat/chat.js';
 export { default as errorpage } from './error/error.js';
