dd5436530ab1a8dc81555500442674074aabcc58
Julius Gerba <juliusgerba@gmail.com>
2024-02-11 08:47:01 +0300

    vite conf upd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+    root: resolve(__dirname, 'src'),
     build: {
-        outDir: 'dist',
+        outDir: '../dist',
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'src/index.html'),
+            },
+        },
     },
     plugins: [handlebars()],
 });
