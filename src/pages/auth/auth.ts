23855b364967d1ed042825b2f09d31151e849ba8
Julius Gerba <juliusgerba@gmail.com>
2024-02-22 09:41:55 +0300

    change form added?

--- src/pages/auth/auth.ts
+++ src/pages/auth/auth.ts
@@ -2,8 +2,13 @@ import tpl from './auth.hbs?raw';
 import Block, { PropsType, ChildrenType } from '../../core/block.js';
 
 export default class AuthPage extends Block {
+    isLogin;
+
     constructor(props: PropsType | ChildrenType) {
         super('main', props);
+
+        this.isLogin = this.props.isLogIn as boolean;
+        this.changeFormInit();
     }
 
     render(): DocumentFragment {
@@ -13,7 +18,14 @@ export default class AuthPage extends Block {
         return this.compile(tpl, propsToRender);
     }
 
-    // signFormHandler() {
-    //     this.props.isLogIn = !this.props.isLogIn;
-    // }
+    changeFormInit() {
+        const btn = this.element!.querySelector('.btn--extra');
+        btn?.addEventListener('click', this.changeForm);
+    }
+
+    changeForm() {
+        console.log(this.isLogin);
+
+        this.props.isLogIn = !this.props.isLogIn;
+    }
 }
