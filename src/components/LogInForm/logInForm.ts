825ce7de342cf8afbcbc999eb146f138eeb18dc3
Julius Gerba <juliusgerba@gmail.com>
2024-02-22 19:05:03 +0300

    feat: auth logic moved to auth class, auth class added

--- src/components/LogInForm/logInForm.ts
+++ src/components/LogInForm/logInForm.ts
@@ -1,11 +1,10 @@
 import tpl from './logInForm.hbs?raw';
-import Block, { PropsType, ChildrenType } from '../../core/block.ts';
+import { PropsType, ChildrenType } from '../../core/block.ts';
+import AuthForm from '../../core/authForm.ts';
 
-export default class LogInForm extends Block {
+export default class LogInForm extends AuthForm {
     constructor(props: PropsType | ChildrenType) {
         super('form', props);
-
-        this.addEvents();
     }
 
     render(): DocumentFragment {
@@ -13,44 +12,6 @@ export default class LogInForm extends Block {
         const propsToRender = (({ events, attr, ...rest }) => rest)(this.props);
 
         return this.compile(tpl, propsToRender);
-    }
-
-    addEvents() {
-        this.element?.addEventListener('submit', event =>
-            this.submitForm(event)
-        );
-    }
-
-    submitForm(event: SubmitEvent) {
-        event.preventDefault();
-        const formIsValid = this.validateForm();
-
-        if (!formIsValid) {
-            console.log('Wrong input values!');
-            return;
-        }
-
-        const formObject: Record<string, string> = {};
-
-        const inputs = this.element!.querySelectorAll('input');
-        inputs.forEach(input => {
-            formObject[input.name] = input.value;
-        });
-
-        console.log(formObject);
-    }
-
-    validateForm(): boolean {
-        let isValid: boolean = true;
-
-        Object.values(this.children).forEach(child => {
-            if (child.element.querySelector('input')) {
-                const inputIsValid = child.validateInput();
-                if (!inputIsValid) isValid = inputIsValid;
-            }
-        });
-
-        return isValid;
     }
 }
 
---
+++ src/core/authForm.ts
@@ -0,0 +1,48 @@
+import Block, { PropsType, ChildrenType } from './block';
+
+export default class AuthForm extends Block {
+    constructor(tagName: string, props: PropsType | ChildrenType) {
+        super(tagName, props);
+
+        this.addEvents();
+    }
+
+    addEvents() {
+        this.element?.addEventListener('submit', event =>
+            this.submitForm(event)
+        );
+    }
+
+    submitForm(event: SubmitEvent) {
+        event.preventDefault();
+        const formIsValid = this.validateForm();
+
+        if (!formIsValid) {
+            console.log('Wrong input values!');
+            return;
+        }
+
+        const formObject: Record<string, string> = {};
+
+        const inputs = this.element!.querySelectorAll('input');
+        inputs.forEach(input => {
+            formObject[input.name] = input.value;
+        });
+
+        console.log(formObject);
+    }
+
+    validateForm(): boolean {
+        let isValid: boolean = true;
+
+        Object.values(this.children).forEach(child => {
+            if (child.element.querySelector('input')) {
+                const inputIsValid = child.validateInput();
+                if (!inputIsValid) isValid = inputIsValid;
+            }
+        });
+
+        return isValid;
+    }
+}
+
