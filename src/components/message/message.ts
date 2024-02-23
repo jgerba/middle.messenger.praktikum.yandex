791ca721f5cd503b56ea48eb0fa1c2bfe4e0b3f2
Julius Gerba <juliusgerba@gmail.com>
2024-02-23 15:10:24 +0300

    feat: message class added

---
+++ src/components/message/message.ts
@@ -0,0 +1,16 @@
+import tpl from './button.hbs?raw';
+import Block, { PropsType } from '../../core/block.js';
+
+export default class Message extends Block {
+    constructor(props: PropsType) {
+        super('article', props);
+    }
+
+    render(): DocumentFragment {
+        // remove events data from props
+        // iife - destructure props from argument, return rest (no unused vars)
+        const propsToRender = (({ events, attr, ...rest }) => rest)(this.props);
+
+        return this.compile(tpl, propsToRender);
+    }
+}
