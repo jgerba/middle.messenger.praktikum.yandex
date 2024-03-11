ae144262bb8f49f6941ccd173edf02f2cb182cb0
Julius Gerba <juliusgerba@gmail.com>
2024-03-11 16:36:45 +0300

    feat: error catch added

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -2,37 +2,72 @@ import AuthAPI from '../api/auth-api';
 import router from '../main';
 
 type DataType = { [key: string]: Record<string, string> };
+type ResponseType = {
+  [key: string]: Record<string, string | { [key: string]: string }> | number;
+};
 
 export default class AuthController {
   static async createUser(submitData: DataType) {
     AuthAPI.createUser(submitData)
-      .then(({ status, response }: DataType) =>
-        console.log({ status, response }),
-      )
-      .then(() => this.getUser());
+      .then(({ status, response }: ResponseType) => {
+        console.log(status, response);
+
+        if (status !== 200) {
+          throw new Error(
+            `${status} ${(response as { [key: string]: string }).reason}`,
+          );
+        }
+
+        this.getUser();
+      })
+      .catch((error) => console.log(error));
   }
 
   static async logIn(submitData: DataType) {
     AuthAPI.logIn(submitData)
-      .then((res: DataType) => console.log(res.status))
-      .then(() => this.getUser());
+      .then(({ status, response }: ResponseType) => {
+        console.log(status);
+
+        if (status !== 200) {
+          throw new Error(
+            `${status} ${(response as { [key: string]: string }).reason}`,
+          );
+        }
+
+        this.getUser();
+      })
+      .catch((error) => console.log(error));
   }
 
   static async getUser() {
     AuthAPI.getUser()
-      .then(({ status, response }: DataType) =>
-        console.log({ status, response }),
-      )
-      .then(() => {
+      .then(({ status, response }: ResponseType) => {
+        console.log(status, response);
+
+        if (status !== 200) {
+          throw new Error(
+            `${status} ${(response as { [key: string]: string }).reason}`,
+          );
+        }
+
         router.go('/messenger');
-      });
+      })
+      .catch((error) => console.log(error));
   }
 
   static async logOut() {
     AuthAPI.logOut()
-      .then((res: DataType) => console.log(res.status))
-      .then(() => {
+      .then(({ status, response }: ResponseType) => {
+        console.log(status);
+
+        if (status !== 200) {
+          throw new Error(
+            `${status} ${(response as { [key: string]: string }).reason}`,
+          );
+        }
+
         router.go('/');
-      });
+      })
+      .catch((error) => console.log(error));
   }
 }
