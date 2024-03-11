eed081aa2ed2363349bb9b4f22970d63e81abded
Julius Gerba <juliusgerba@gmail.com>
2024-03-11 18:17:55 +0300

    feat: user api/ctrl added

---
+++ src/api/user-api.ts
@@ -0,0 +1,23 @@
+import HTTPTransport from '../core/HTTPTransport';
+
+type OptionsType = { [key: string]: Record<string, string> };
+
+const api = new HTTPTransport('https://ya-praktikum.tech/api/v2/user');
+
+export default class UserAPI {
+  static async changeUser(options: OptionsType) {
+    return api.put('/profile', options);
+  }
+
+  static async changeAvatar(options: OptionsType) {
+    return api.put('/profile/avatar', options);
+  }
+
+  static async changePassword(options: OptionsType) {
+    return api.put('/password', options);
+  }
+
+  static async searchUser(options: OptionsType) {
+    return api.post('/user/search', options);
+  }
+}
---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,67 @@
+import UserAPI from '../api/user-api';
+
+type DataType = { [key: string]: Record<string, string> };
+type ResponseType = {
+  [key: string]: Record<string, string | { [key: string]: string }> | number;
+};
+
+export default class UserController {
+  static async changeUser(submitData: DataType) {
+    UserAPI.changeUser(submitData)
+      .then(({ status, response }: ResponseType) => {
+        console.log(status, response);
+
+        if (status !== 200) {
+          throw new Error(
+            `${status} ${(response as { [key: string]: string }).reason}`,
+          );
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  static async changeAvatar(submitData: DataType) {
+    UserAPI.changeAvatar({
+      ...submitData,
+      headers: { 'Content-Type': 'multipart/form-data' },
+    })
+      .then(({ status, response }: ResponseType) => {
+        console.log(status, response);
+
+        if (status !== 200) {
+          throw new Error(
+            `${status} ${(response as { [key: string]: string }).reason}`,
+          );
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  static async changePassword(submitData: DataType) {
+    UserAPI.changePassword(submitData)
+      .then(({ status, response }: ResponseType) => {
+        console.log(status);
+
+        if (status !== 200) {
+          throw new Error(
+            `${status} ${(response as { [key: string]: string }).reason}`,
+          );
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+
+  static async searchUser(submitData: DataType) {
+    UserAPI.searchUser(submitData)
+      .then(({ status, response }: ResponseType) => {
+        console.log(status, response);
+
+        if (status !== 200) {
+          throw new Error(
+            `${status} ${(response as { [key: string]: string }).reason}`,
+          );
+        }
+      })
+      .catch((error) => console.log(error));
+  }
+}
