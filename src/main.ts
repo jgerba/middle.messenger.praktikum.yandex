9937bd1618c92e48ef56a836d91a9317d31cf245
Julius Gerba <juliusgerba@gmail.com>
2024-03-17 08:20:58 +0300

    refactor: app class added

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Router from './core/Router.ts';
 import authController from './controllers/auth-controller.ts';
+import store from './core/store.ts';
 
 import signUpPage from './pages/signUp/index.ts';
 import signInPage from './pages/signIn/index.ts';
@@ -7,7 +8,7 @@ import signInPage from './pages/signIn/index.ts';
 import './styles/style.scss';
 
 class App {
-  appRouter: Router;
+  router: Router;
 
   constructor() {
     this.initRouter();
@@ -15,20 +16,19 @@ class App {
   }
 
   initRouter() {
-    this.appRouter = new Router();
+    this.router = new Router();
     this.router.use('/', signInPage);
     this.router.use('/sign-up', signUpPage);
     this.router.start();
+
+    store.setRouter(this.router);
   }
 
   async checkAuth() {
     await authController.getUser();
   }
-
-  get router() {
-    return this.appRouter;
-  }
 }
 
-const app = new App();
-export default app.router;
+document.addEventListener('DOMContentLoaded', () => {
+  new App();
+});
