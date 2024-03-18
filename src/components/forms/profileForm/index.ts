01cc6b5f7ef1930b6e5d7bd301e98088ae518fe9
Julius Gerba <juliusgerba@gmail.com>
2024-03-18 10:04:57 +0300

    upd

--- src/components/forms/profileForm/index.ts
+++ src/components/forms/profileForm/index.ts
@@ -9,42 +9,42 @@ type IndexedType = {
   [key: string]: string | number | IndexedType;
 };
 
-const user = store.getState().user as IndexedType;
+const user = store.getState().user as IndexedType | undefined;
 
 export default new ProfileForm({
   emailInput: new FormInput({
     name: 'email',
     text: 'Email',
     type: 'email',
-    value: user.email as string,
+    value: (user?.email as string) || '',
     regExpString: '^[a-zA-Z0-9._-]+@[a-zA-Z]+\\.[a-zA-Z]{2,}$',
     attr: { class: 'input-wrapper' },
   }),
   loginInput: new FormInput({
     name: 'login',
     text: 'Login',
-    value: user.login as string,
+    value: (user?.login as string) || '',
     regExpString: '^(?=.*[A-Za-z])[-_A-Za-z0-9]{3,20}$',
     attr: { class: 'input-wrapper' },
   }),
   nameInput: new FormInput({
     name: 'first_name',
     text: 'Name',
-    value: user.first_name as string,
+    value: (user?.first_name as string) || '',
     regExpString: '^[A-ZА-Я][a-zа-я-]+$',
     attr: { class: 'input-wrapper' },
   }),
   surnameInput: new FormInput({
     name: 'second_name',
     text: 'Surname',
-    value: user.second_name as string,
+    value: (user?.second_name as string) || '',
     regExpString: '^[A-ZА-Я][a-zа-я-]+$',
     attr: { class: 'input-wrapper' },
   }),
   displayNameInput: new FormInput({
     name: 'display_name',
     text: 'Username',
-    value: user.display_name as string,
+    value: (user?.display_name as string) || '',
     regExpString: '^(?=.*[A-Za-z])[-_A-Za-z0-9]{3,20}$',
     attr: { class: 'input-wrapper' },
   }),
@@ -52,7 +52,7 @@ export default new ProfileForm({
     name: 'phone',
     text: 'Phone',
     type: 'phone',
-    value: user.phone as string,
+    value: (user?.phone as string) || '',
     regExpString: '^\\+?\\d{10,15}$',
     attr: { class: 'input-wrapper' },
   }),
