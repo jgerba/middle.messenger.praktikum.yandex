aa30524897f632b6a50ca9108478a383b5be7fa2
Julius Gerba <juliusgerba@gmail.com>
2024-03-31 09:45:53 +0300

    feat: add userListitem component

---
+++ src/components/userListItem/userListItem.ts
@@ -0,0 +1,17 @@
+import tpl from './userListItem.hbs?raw';
+import Block from '../../core/block.js';
+import { ChildrenType, PropsType } from '../../core/types.js';
+
+export default class UserListItem extends Block {
+  constructor(props: PropsType | ChildrenType) {
+    super('li', props);
+  }
+
+  render(): DocumentFragment {
+    // remove events & attr data from props
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const propsToRender = (({ events, attr, ...rest }) => rest)(this.props);
+
+    return this.compile(tpl, propsToRender);
+  }
+}
