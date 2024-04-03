3c8652acf7d291bad927d58f785cf82ae10b349e
Julius Gerba <juliusgerba@gmail.com>
2024-04-03 13:00:17 +0300

    feat: add popup component & logic

--- src/core/types.ts
+++ src/core/types.ts
@@ -15,7 +15,7 @@ export type PropsType = Record<string, PropValue | PropValue[]>;
 export type ChildrenType = Record<string, Block | Block[]>;
 
 export type IndexedType = {
-  [key: string]: string | number | IndexedType;
+  [key: string]: string | number | boolean | IndexedType;
 };
 
 export type DataType = {
@@ -34,3 +34,5 @@ export type AddUsersDataType = {
 export type ResponseType = {
   [key: string]: Record<string, string | { [key: string]: string }> | number;
 };
+
+export type PopUpType = { message: string; isError?: boolean };
