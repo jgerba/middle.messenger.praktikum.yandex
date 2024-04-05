7aa6fee531e7316e448ecfdeaaad647d1c22f7cb
Julius Gerba <juliusgerba@gmail.com>
2024-04-05 22:11:16 +0300

    feat: add send location logic

--- src/components/dropdowns/messageDropdown/messageDropdown.ts
+++ src/components/dropdowns/messageDropdown/messageDropdown.ts
@@ -1,9 +1,13 @@
 import tpl from './messageDropdown.hbs?raw';
 import Block from '../../../core/block.ts';
 
+import store from '../../../core/store.ts';
+import WSController from '../../../controllers/WS-controller.ts';
+
 import Dropdown from '../dropdown.ts';
 
 import { PropsType, ChildrenType } from '../../../core/types.ts';
+import { MSG, POP_MSG } from '../../../core/const.ts';
 
 export default class MessageDropdown extends Dropdown {
   constructor(props: PropsType | ChildrenType) {
@@ -26,10 +30,7 @@ export default class MessageDropdown extends Dropdown {
       this.addMedia.bind(this),
     );
     (this.children.fileBtn as Block).addEvent('click', this.addFile.bind(this));
-    (this.children.locationBtn as Block).addEvent(
-      'click',
-      this.addLocation.bind(this),
-    );
+    (this.children.locationBtn as Block).addEvent('click', this.sendLocation);
   }
 
   private addMedia() {
@@ -40,7 +41,36 @@ export default class MessageDropdown extends Dropdown {
     console.log('Adding file...');
   }
 
-  private addLocation() {
-    console.log('Adding location...');
+  private sendLocation() {
+    /* eslint no-inner-declarations: 0 */
+
+    if (navigator.geolocation) {
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      };
+
+      const success = (pos: GeolocationPosition) => {
+        const crd = pos.coords as GeolocationCoordinates;
+        WSController.sendMessage({
+          message: `${MSG.GEO}latitude: ${crd.latitude}, longitude: ${crd.longitude}`,
+        });
+      };
+
+      function error(err: GeolocationPositionError) {
+        store.setState('popUp', {
+          message: `${err.code} ${err.message}`,
+          isError: true,
+        });
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    } else {
+      store.setState('popUp', {
+        message: POP_MSG.GEO_FAIL,
+        isError: true,
+      });
+    }
   }
 }
