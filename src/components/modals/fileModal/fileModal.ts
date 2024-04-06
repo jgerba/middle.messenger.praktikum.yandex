515d886239f7ee34c944ffb916320f42892086fd
Julius Gerba <juliusgerba@gmail.com>
2024-04-06 18:17:50 +0300

    refactor: move avatar modals to standalone components

--- src/components/modals/fileModal/fileModal.ts
+++ src/components/modals/fileModal/fileModal.ts
@@ -2,20 +2,12 @@ import tpl from './fileModal.hbs?raw';
 import Block from '../../../core/block.ts';
 import Modal from '../modal.ts';
 
-import store from '../../../core/store.ts';
-import userController from '../../../controllers/user-controller.ts';
-import chatsController from '../../../controllers/chats-controller.ts';
-
 import { PropsType, ChildrenType } from '../../../core/types.ts';
 
 export default class FileModal extends Modal {
-  // define _origin of modal: chat/user
-  private _origin: string;
-
   constructor(props: PropsType | ChildrenType) {
     super(props);
 
-    this._origin = props.origin as string;
     this.initFileInput();
   }
 
@@ -60,29 +52,7 @@ export default class FileModal extends Modal {
     this.fetchHandler(inputFiles);
   }
 
-  private async fetchHandler(inputFiles: FileList) {
-    const formData = new FormData();
-    let status: number;
-
-    formData.append('avatar', inputFiles[0]);
-
-    if (this._origin === 'chat') {
-      const chatId = (store.getState().currentChat as { [key: string]: number })
-        .id;
-
-      formData.append('chatId', chatId.toString());
-
-      status = (await chatsController.changeAvatar({
-        data: formData,
-      })) as number;
-    } else {
-      status = (await userController.changeAvatar({
-        data: formData,
-      })) as number;
-    }
-
-    if (status === 200) {
-      this.closeModal();
-    }
+  protected async fetchHandler(inputFiles: FileList) {
+    console.log(inputFiles);
   }
 }
