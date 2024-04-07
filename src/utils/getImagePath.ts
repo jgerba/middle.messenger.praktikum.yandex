5b8c39b08e1933f3d974977241a519933557ba49
Julius Gerba <juliusgerba@gmail.com>
2024-04-07 09:52:47 +0300

    fix: upd regexp pattern

--- src/utils/getImagePath.ts
+++ src/utils/getImagePath.ts
@@ -1,7 +1,10 @@
 import { MSG_KEYS } from '../core/const.ts';
 
 export default function getImagePath(str: string): string | null {
-  const regex = new RegExp(`${MSG_KEYS.IMAGE_KEY}/([^\r\n]+.jpg)`);
+  const regex = new RegExp(
+    `${MSG_KEYS.IMAGE_KEY}/([^\r\n]+.(jpg|jpeg|png|gif|webp))`,
+    'i',
+  );
 
   const match = str.match(regex);
 
